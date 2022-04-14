dfbb2651fc7fee90d26632107af0b5977b4ebe38
Jerry Chong <jerrychong25@gmail.com>
2022-04-14 20:22:02 +0800

    Fixed Home Page, Usage Page and PV Cells Page Back Button Issue

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,70 +8,104 @@ class MyHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox.expand(
-        // -> 01
-        child: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-            image: AssetImage('assets/images/app_background.png'),
-            fit: BoxFit.cover, // -> 02
-          )),
-          padding: const EdgeInsets.all(20.0),
+    return WillPopScope(
+      child: Scaffold(
+        body: SizedBox.expand(
+          // -> 01
           child: Container(
-            color: Colors.black,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/logo.png',
-                  height: 300,
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                Text(
-                  'Your one app for energy management',
-                  style: TextStyle(
-                    fontSize: 22.0,
-                    color: Colors.white,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/images/app_background.png'),
+              fit: BoxFit.cover, // -> 02
+            )),
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              color: Colors.black,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/logo.png',
+                    height: 300,
                   ),
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  'opportunities and smart',
-                  style: TextStyle(
-                    fontSize: 22.0,
-                    color: Colors.white,
+                  const SizedBox(
+                    height: 40,
                   ),
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  'eco-living ideas',
-                  style: TextStyle(
-                    fontSize: 22.0,
-                    color: Colors.white,
+                  Text(
+                    'Your one app for energy management',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.pushReplacementNamed(context, '/usage');
-                  },
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20.0),
-                    child: Image.asset('assets/images/start_button.png',
-                        width: 150, height: 150),
+                  Text(
+                    'opportunities and smart',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                ),
-              ],
+                  Text(
+                    'eco-living ideas',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pushReplacementNamed(context, '/usage');
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: Image.asset('assets/images/start_button.png',
+                          width: 150, height: 150),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
+      onWillPop: () async {
+        final value = await showDialog<bool>(
+            context: context,
+            builder: (contex) {
+              return AlertDialog(
+                  title: const Text('Alert'),
+                  content: const Text('Do you want to exit this app?'),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () => Navigator.of(context).pop(false),
+                      child: const Text('No'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.black, // background
+                        onPrimary: Colors.white, // foreground
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => Navigator.of(context).pop(true),
+                      child: const Text('Exit'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.black, // background
+                        onPrimary: Colors.white, // foreground
+                      ),
+                    ),
+                  ]);
+            });
+        if (value != null) {
+          return Future.value(value);
+        } else {
+          return Future.value(false);
+        }
+      },
     );
   }
 }
